0be20dd64a04ab1f883bcb3f34ad2d02179601e9
Tom Muller <thomasamuller@gmail.com>
2022-09-13 10:55:15 +1000

    new svelte app

---
+++ src/words.js
@@ -0,0 +1,451 @@
+export default {
+    'music': {
+        "8bit": {
+            "is_adjective": "TRUE"
+        },
+        "Acid": {
+            "is_adjective": "TRUE"
+        },
+        "Acoustic": {
+            "is_adjective": ""
+        },
+        "African": {
+            "is_adjective": ""
+        },
+        "Ambient": {
+            "is_adjective": ""
+        },
+        "Anime": {
+            "is_adjective": ""
+        },
+        "Anti-Folk": {
+            "is_adjective": ""
+        },
+        "Art Punk": {
+            "is_adjective": ""
+        },
+        "Asian": {
+            "is_adjective": "TRUE"
+        },
+        "Avant-Garde": {
+            "is_adjective": ""
+        },
+        "Background": {
+            "is_adjective": ""
+        },
+        "Ballet": {
+            "is_adjective": ""
+        },
+        "Baroque": {
+            "is_adjective": ""
+        },
+        "Bass": {
+            "is_adjective": "TRUE"
+        },
+        "Bebop": {
+            "is_adjective": ""
+        },
+        "Bluegrass": {
+            "is_adjective": ""
+        },
+        "Blues": {
+            "is_adjective": ""
+        },
+        "Bossa Nova": {
+            "is_adjective": ""
+        },
+        "Bouncy": {
+            "is_adjective": "TRUE"
+        },
+        "Breakbeat": {
+            "is_adjective": ""
+        },
+        "Britpunk": {
+            "is_adjective": ""
+        },
+        "Brostep": {
+            "is_adjective": ""
+        },
+        "Cantata": {
+            "is_adjective": ""
+        },
+        "Chamber": {
+            "is_adjective": ""
+        },
+        "Chant": {
+            "is_adjective": ""
+        },
+        "Children’s": {
+            "is_adjective": "TRUE"
+        },
+        "Chill": {
+            "is_adjective": "TRUE"
+        },
+        "Chiptune": {
+            "is_adjective": ""
+        },
+        "Choral": {
+            "is_adjective": ""
+        },
+        "Classical": {
+            "is_adjective": ""
+        },
+        "Club": {
+            "is_adjective": ""
+        },
+        "Commercial": {
+            "is_adjective": ""
+        },
+        "Concerto": {
+            "is_adjective": ""
+        },
+        "Country": {
+            "is_adjective": ""
+        },
+        "Cowpunk": {
+            "is_adjective": ""
+        },
+        "Crunk": {
+            "is_adjective": ""
+        },
+        "Cuban": {
+            "is_adjective": "TRUE"
+        },
+        "Dance": {
+            "is_adjective": ""
+        },
+        "Dancehall": {
+            "is_adjective": ""
+        },
+        "Dark": {
+            "is_adjective": "TRUE"
+        },
+        "Deep": {
+            "is_adjective": "TRUE"
+        },
+        "Digital": {
+            "is_adjective": "TRUE"
+        },
+        "Disco": {
+            "is_adjective": ""
+        },
+        "Disney": {
+            "is_adjective": ""
+        },
+        "Dixieland": {
+            "is_adjective": ""
+        },
+        "Doom": {
+            "is_adjective": "TRUE"
+        },
+        "Downtempo": {
+            "is_adjective": ""
+        },
+        "Dream": {
+            "is_adjective": "TRUE"
+        },
+        "Drone": {
+            "is_adjective": ""
+        },
+        "Drum’n’bass": {
+            "is_adjective": ""
+        },
+        "Dub": {
+            "is_adjective": ""
+        },
+        "Dubstep": {
+            "is_adjective": ""
+        },
+        "Easy Listening": {
+            "is_adjective": ""
+        },
+        "Electric": {
+            "is_adjective": "TRUE"
+        },
+        "Elevator": {
+            "is_adjective": ""
+        },
+        "Emo": {
+            "is_adjective": ""
+        },
+        "Exercise": {
+            "is_adjective": ""
+        },
+        "Experimental Rock": {
+            "is_adjective": ""
+        },
+        "Expressionist": {
+            "is_adjective": ""
+        },
+        "Folk": {
+            "is_adjective": ""
+        },
+        "Freestyle": {
+            "is_adjective": "TRUE"
+        },
+        "Funk": {
+            "is_adjective": ""
+        },
+        "Game Boy": {
+            "is_adjective": ""
+        },
+        "Gangsta": {
+            "is_adjective": "TRUE"
+        },
+        "Garage": {
+            "is_adjective": ""
+        },
+        "Glam": {
+            "is_adjective": ""
+        },
+        "Glitch": {
+            "is_adjective": ""
+        },
+        "Golden Age": {
+            "is_adjective": ""
+        },
+        "Gospel": {
+            "is_adjective": ""
+        },
+        "Goth": {
+            "is_adjective": ""
+        },
+        "Grime": {
+            "is_adjective": ""
+        },
+        "Grunge": {
+            "is_adjective": ""
+        },
+        "Hardcore": {
+            "is_adjective": ""
+        },
+        "Hardstyle": {
+            "is_adjective": ""
+        },
+        "Harmonic": {
+            "is_adjective": "TRUE"
+        },
+        "Hill Country": {
+            "is_adjective": ""
+        },
+        "Hip Pop": {
+            "is_adjective": ""
+        },
+        "Hip-Hop": {
+            "is_adjective": ""
+        },
+        "Honky Tonk": {
+            "is_adjective": ""
+        },
+        "House": {
+            "is_adjective": ""
+        },
+        "Impressionist": {
+            "is_adjective": ""
+        },
+        "Industrial": {
+            "is_adjective": ""
+        },
+        "Jazz": {
+            "is_adjective": ""
+        },
+        "Karaoke": {
+            "is_adjective": ""
+        },
+        "K-Pop": {
+            "is_adjective": ""
+        },
+        "Liquid": {
+            "is_adjective": "TRUE"
+        },
+        "Lo-fi": {
+            "is_adjective": ""
+        },
+        "Lounge": {
+            "is_adjective": ""
+        },
+        "Lullaby": {
+            "is_adjective": ""
+        },
+        "Math": {
+            "is_adjective": "TRUE"
+        },
+        "Medieval": {
+            "is_adjective": ""
+        },
+        "Metal": {
+            "is_adjective": ""
+        },
+        "Minimalism": {
+            "is_adjective": ""
+        },
+        "Minimalist": {
+            "is_adjective": "TRUE"
+        },
+        "Modern": {
+            "is_adjective": "TRUE"
+        },
+        "Musique Concrète": {
+            "is_adjective": ""
+        },
+        "New Orlean Blues": {
+            "is_adjective": ""
+        },
+        "New Wave": {
+            "is_adjective": ""
+        },
+        "Noise": {
+            "is_adjective": ""
+        },
+        "Novelty": {
+            "is_adjective": ""
+        },
+        "Opera": {
+            "is_adjective": ""
+        },
+        "Oratorio": {
+            "is_adjective": ""
+        },
+        "Orchestral": {
+            "is_adjective": ""
+        },
+        "Organum": {
+            "is_adjective": ""
+        },
+        "Piano": {
+            "is_adjective": "TRUE"
+        },
+        "Pop": {
+            "is_adjective": ""
+        },
+        "Post": {
+            "is_adjective": "TRUE"
+        },
+        "Power": {
+            "is_adjective": "TRUE"
+        },
+        "Progressive": {
+            "is_adjective": ""
+        },
+        "Punk": {
+            "is_adjective": ""
+        },
+        "Punta": {
+            "is_adjective": ""
+        },
+        "R&B": {
+            "is_adjective": ""
+        },
+        "Ragtime": {
+            "is_adjective": ""
+        },
+        "Rap": {
+            "is_adjective": ""
+        },
+        "Rave": {
+            "is_adjective": ""
+        },
+        "Reggae": {
+            "is_adjective": ""
+        },
+        "Renaissance": {
+            "is_adjective": ""
+        },
+        "Rhythm": {
+            "is_adjective": "TRUE"
+        },
+        "Rock": {
+            "is_adjective": ""
+        },
+        "Shoegaze": {
+            "is_adjective": ""
+        },
+        "Ska": {
+            "is_adjective": ""
+        },
+        "Sonata": {
+            "is_adjective": ""
+        },
+        "Soul": {
+            "is_adjective": ""
+        },
+        "Space": {
+            "is_adjective": "TRUE"
+        },
+        "Spiritual": {
+            "is_adjective": ""
+        },
+        "Spoken Word": {
+            "is_adjective": ""
+        },
+        "Steampunk": {
+            "is_adjective": ""
+        },
+        "String Quartet": {
+            "is_adjective": ""
+        },
+        "Surf": {
+            "is_adjective": "TRUE"
+        },
+        "Swing": {
+            "is_adjective": ""
+        },
+        "Symphonic": {
+            "is_adjective": "TRUE"
+        },
+        "Synth": {
+            "is_adjective": ""
+        },
+        "Techno": {
+            "is_adjective": ""
+        },
+        "Thrash": {
+            "is_adjective": "TRUE"
+        },
+        "Timba": {
+            "is_adjective": ""
+        },
+        "Trance": {
+            "is_adjective": ""
+        },
+        "Trap": {
+            "is_adjective": ""
+        },
+        "Tribal": {
+            "is_adjective": "TRUE"
+        },
+        "Trip Hop": {
+            "is_adjective": ""
+        },
+        "Tropical": {
+            "is_adjective": "TRUE"
+        },
+        "TV Theme": {
+            "is_adjective": ""
+        },
+        "Twelve-tone": {
+            "is_adjective": ""
+        },
+        "Vaporwave": {
+            "is_adjective": ""
+        },
+        "Vaudeville": {
+            "is_adjective": ""
+        },
+        "Viking": {
+            "is_adjective": "TRUE"
+        },
+        "Vocal": {
+            "is_adjective": "TRUE"
+        },
+        "Workout": {
+            "is_adjective": ""
+        },
+        "World": {
+            "is_adjective": ""
+        },
+        "Yodelling": {
+            "is_adjective": ""
+        }
+    }
+}
